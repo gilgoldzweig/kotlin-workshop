2e8b31ec7ccd41cb979a546ff34fcddded8287d7
Gil Goldzweig Goldbaum <gil@10bis.co.il>
2019-10-01 12:22:35 +0300

    Added ranges example

--- src/main/kotlin/day1/lecture2/Ranges.kt
+++ src/main/kotlin/day1/lecture2/Ranges.kt
@@ -1,2 +1,18 @@
 package day1.lecture2
 
+/**
+ * Create two ranges
+ *      numbers between 1 - 20
+ *      letters between 'a' - 'q'
+ *
+ * Check if a number lies within a range.
+ * Check if a letter lies within a range.
+ *
+ * Iterate over the ranges you created earlier
+ * Iterate over the ranges you created earlier with jumps of 2
+ * Iterate over the ranges you created earlier backwards
+ */
+fun main() {
+    //Write your code below this line
+
+}
