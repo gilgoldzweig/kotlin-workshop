e6a620c680e4b0eff1320d01f73db16243e3d9d1
Gil Goldzweig Goldbaum <gil@10bis.co.il>
2019-10-01 12:42:06 +0300

    Added practise for Expressions.kt

--- src/main/kotlin/day1/lecture2/Expressions.kt
+++ src/main/kotlin/day1/lecture2/Expressions.kt
@@ -1,2 +1,44 @@
 package day1.lecture2
 
+/**
+ * Fill in the functions below
+ */
+fun main() {
+    assert(returnLastNameWithIf("Hadas") == "Peled")
+    assert(returnLastNameWithIf("Gil") == "Unknown")
+
+    assert(returnLastNameWithWhenStatment("Hadas") == "Peled")
+    assert(returnLastNameWithWhenStatment("Gil") == "Goldzweig")
+    assert(returnLastNameWithWhenStatment("Eden") == "Bugdary")
+    assert(returnLastNameWithWhenStatment("Refael") == "Unknown")
+}
+
+/**
+ * return the person last name using an [if] expression based on first name
+ *
+ * @return "Peled" if first name is "Hadas"
+ * @return "Unknown" if anything else
+ */
+fun returnLastNameWithIf(firstName: String): String {
+    //Write your code below this line
+    return if (firstName) {
+
+    } else {
+
+    }
+}
+
+/**
+ * return the person last name using a [when] expression based on first name
+ *
+ * @return "Peled" if first name is "Hadas"
+ * @return "Goldzweig" if first name is "Gil"
+ * @return "Bugdary" if first name is "Eden"
+ * @return "Unknown" if the first name is not "Gil" or "Hadas" or "Eden"
+ */
+fun returnLastNameWithWhenStatment(firstName: String): String {
+    //Write your code below this line
+    return when (firstName) {
+
+    }
+}
