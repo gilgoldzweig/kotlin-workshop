f2fea3a89c1c9e2beb44bcc6b4c9f9153c6edcbc
hadaspeled <36445459+hadaspeled@users.noreply.github.com>
2019-10-11 17:29:31 +0300

    pairs

---
+++ src/main/kotlin/day2/lecture3/exercises/1_Pairs.kt
@@ -0,0 +1,23 @@
+package day2.lecture3.exercises
+
+/**
+ * Let's go through an example of getting information about a book in the format of a Pair.
+ * Generally, you want information about both the title and the author, and perhaps also the year.
+ *
+ * Let’s create a basic book class, with a title, author, and year.
+ * Of course, you could get each of the properties separately.
+ *
+ * Create a method that returns both the title and the author as a Pair.
+ * Create a method that returns the title, author and year as a Triple.
+ *
+ * Use the documentation to find out how to use Triple.
+ *
+ * Create a book instance.
+ *
+ * Print out the information about the book in a sentence,
+ * such as: “Here is your book X written by Y in Z.”
+ *
+ */
+fun main() {
+    //Write your code below this line
+}
