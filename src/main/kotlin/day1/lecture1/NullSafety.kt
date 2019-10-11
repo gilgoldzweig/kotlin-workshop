d5e13f418ec0e9149432551ee85b66b6099be85d
hadaspeled <36445459+hadaspeled@users.noreply.github.com>
2019-10-11 15:31:53 +0300

    nullsafety practice

--- src/main/kotlin/day1/lecture1/NullSafety.kt
+++ src/main/kotlin/day1/lecture1/NullSafety.kt
@@ -1,2 +1,12 @@
 package day1.lecture1
 
+/**
+ * Create a nullable integer variable
+ *
+ * Use a null-check that increases the value by one if it's not null,
+ * otherwise returns 0, and prints the result.
+ *
+ */
+fun main() {
+    // Write your code below this line
+}
