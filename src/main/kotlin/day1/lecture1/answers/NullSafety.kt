064cc32c6e8d81bc7112a19a429eb1bee0866d98
hadaspeled <36445459+hadaspeled@users.noreply.github.com>
2019-10-11 16:00:18 +0300

    nullsafety answer

--- src/main/kotlin/day1/lecture1/answers/NullSafety.kt
+++ src/main/kotlin/day1/lecture1/answers/NullSafety.kt
@@ -1,2 +1,13 @@
 package day1.lecture1.answers
 
+/**
+ * Create a nullable integer variable
+ *
+ * Use a null-check that increases the value by one if it's not null,
+ * otherwise returns 0, and prints the result.
+ *
+ */
+fun main() {
+    val nullableInt :Int? = 10
+    println("${nullableInt?.plus(1) ?: 0}")
+}
