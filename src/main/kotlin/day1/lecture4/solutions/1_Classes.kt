d4edbe6239173b963fb823261da2eedd13e2138a
hadaspeled <36445459+hadaspeled@users.noreply.github.com>
2019-10-11 16:36:08 +0300

    stuff

---
+++ src/main/kotlin/day1/lecture4/solutions/1_Classes.kt
@@ -0,0 +1,26 @@
+package day1.lecture4.solutions
+
+/**
+ *
+ * Create class, SimpleSpice.
+ *
+ * Let the class be a property with a String for the name of the spice,
+ * and a String for the level of spiciness.
+ *
+ * Set the name to "curry" and the spiciness to "mild".
+ *
+ * Using a string for spiciness is nice for users, but not useful for calculations.
+ *
+ * Add a heat property to your class with a getter that returns a numeric value for spiciness.
+ * Use a value of 5 for mild.
+ *
+ * Add an init block that prints out its name and heat.
+ *
+ * Create an instance of SimpleSpice.
+ *
+ */
+
+fun main() {
+}
+
+class SimpleSpice(val name: String = "curry", val spiciness: String = "mild")
