09749b9b1353f3fed37cffdfa1339ca01618da13
hadaspeled <36445459+hadaspeled@users.noreply.github.com>
2019-10-11 16:23:49 +0300

    when exercise + solution

---
+++ src/main/kotlin/day1/lecture2/exercises/1_When.kt
@@ -0,0 +1,15 @@
+package day1.lecture2.exercises
+
+/**
+ * Create a when statement with three comparisons:
+ *
+ * Create a string variable with your name
+ *
+ * If the length of your name is 0, print an error message.
+ * If the length is longer than 2, but shorter than 12, print your name and the length.
+ * If it's anything else, print "OK name".
+ *
+ */
+fun main() {
+    //Write your code below this line
+}
--- src/main/kotlin/day1/lecture2/solutions/empty.kt
+++
@@ -1,2 +0,0 @@
-package day1.lecture2.solutions
-
