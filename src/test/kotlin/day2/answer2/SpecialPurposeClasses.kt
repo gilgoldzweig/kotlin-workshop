0298ed40092ce1306de92dd1c2ec524d8fe3727a
Gil Goldzweig Goldbaum <gil@10bis.co.il>
2019-10-11 15:34:09 +0300

    Fixed functions file

---
+++ src/test/kotlin/day2/answer2/SpecialPurposeClasses.kt
@@ -0,0 +1,53 @@
+package day2.lecture2
+
+
+/**
+ * Create a singleton class named "Counter" the class should have
+ *
+ *   1. private mutable variable [Int] named [counter] with a starting value of 0
+ *   2. a single line function called [currentCount] which returns the value of [counter]
+ *   3. a function called [increment] which adds 1 to the [counter] variable
+ *
+*/
+
+//__________________Write your singleton here______________
+
+/**
+ * Create an enum class named "SolarSystem" the class needs to be filled with the following information
+ *
+ *
+ * Mercury
+ * Venus
+ * Earth
+ * Mars
+ * Jupiter
+ * Saturn
+ * Uranus
+ * Neptune
+ *
+ * EXTRA: add a radius parameter to the enum class and fill the stars with the radius
+ *
+ * Mercury = 2,439.7
+ * Venus = 6,051.8
+ * Earth = 6,371
+ * Mars = 3,389.5
+ * Jupiter = 69,911
+ * Saturn = 58,232
+ * Uranus = 25,362
+ * Neptune = 24,622
+ */
+
+//__________________Write your enum class here______________
+
+
+/**
+ * Write a sealed class named State
+ * with four sub classes
+ *
+ * [Idle]
+ * [Loading]
+ * [Completed]
+ * [Error]
+ */
+
+//__________________Write your sealed class here______________
---
+++ src/test/kotlin/day3/answer1/file.kt
@@ -0,0 +1,2 @@
+package day3.answer1
+
