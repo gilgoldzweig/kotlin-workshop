a4913605407fa8d7d9716a7932dc57f047fbd1da
hadaspeled <36445459+hadaspeled@users.noreply.github.com>
2019-10-12 09:50:26 +0300

    Jvm

--- src/main/kotlin/day3/lecture3/solutions/MyClass.java
+++ src/main/kotlin/day3/lecture3/solutions/MyClass.java
@@ -5,6 +5,19 @@ package day3.lecture3.solutions;
 public class MyClass {
 
     public static void main(String[] args) {
-        new Bear();
+//        Bear bear = new Bear();
+        Colors.INSTANCE.getRandomColor();
+        Bear.Companion.getDefaultColor();
+//        Bear.Companion.getBearColor();
+
+        Colors.getRandomColor();
+        Bear.getDefaultColor();
+//        Bear.getBearColor();
+
+
+
+
+        new Bear("Yellow", "Salmon",  8);
+        new Bear( "Salmon",  8);
     }
 }
